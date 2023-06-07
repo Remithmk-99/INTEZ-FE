2efff5ff16c8ec0e3d38d9916a51422ed4ebf621
remithmk <remith@stunntech.com>
2023-06-07 11:42:40 +0530

    weekly dates for timesheet completed

--- src/app/project-modules/services/api.service.ts
+++ src/app/project-modules/services/api.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-// import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-//   baseUrl = environment.apiUrl
-baseUrl = ''
+  baseUrl = environment.apiUrl
 
   constructor(private http: HttpClient) { }
 
